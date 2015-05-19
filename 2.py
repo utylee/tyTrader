c8f672800540d08ee449ad0acd173b53cbeb632a
utylee <utylee@gmail.com>
2015-05-19 11:00:20 +0900

    2.py

--- 2.py
+++ 2.py
@@ -1,10 +1,12 @@
 
 import os
-
+true false
+False
+a = True
 if __name__ == "__main__":
     pass
 
 for i in list:
-    print('i luv u dong hee')
+    print('아 정말 키보드 타이핑이 이렇게 즐거운 일이라는 것을 새삼 다시 깨닫고 있는 중입니다')
 
 print('hello utylee')
