765f7facf8ed367ae7eedc0cd82abcc7df29fc14
utylee <utylee@gmail.com>
2015-07-21 14:13:13 +0900

    modied : rename to eng txt / modied : some changing

--- 99-test/pyqt-qml-view.py
+++ 99-test/pyqt-qml-view.py
@@ -2,21 +2,39 @@ import sys
 import asyncio
 import time
 
-from quamash import QEventLoop
+from quamash import QEventLoop, QThreadExecutor
+from win32gui import SetWindowPos
+import win32con
 
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine
+from PyQt5.QtCore import QUrl
+from PyQt5.QtQuick import QQuickView, QQuickWindow
 
 app = QGuiApplication(sys.argv)
 loop = QEventLoop(app)
 engine = QQmlApplicationEngine("main.qml")
+
+#아래에서 리턴받는 객체는 QQuickWindow였습니다. 
 window = engine.rootObjects()[0]
-#window = engine.rootObjects()
 window.show()
 
 asyncio.set_event_loop(loop)
 
 
+
+# 바 클래스를 스케치해봅니다.
+class CCandle:
+    def __init__(self):
+        self.price = 1000
+        self.time = 0 
+
+    # 가격을 설정합니다.
+    def setPrice(self, price):
+        # qml 상의 메써드를 호출합니다
+        pass
+
+#QQuickWindow.parent.
 @asyncio.coroutine
 def printInterval():
 
@@ -26,9 +44,14 @@ def printInterval():
         curtime = loop.time()
         elaptime = curtime - inittime
         #print(elaptime)
-        if elaptime > 1:
+        if elaptime > 2:
             print("{}".format(elaptime))
             inittime = curtime
+            #loop.call_soon_threadsafe(activate)
+
+            #천신만고 끝에 activeWindow 대신 아래 함수를 사용해야한다는 것을
+            # 깨달았습니다.
+            window.requestActivate()
 
         yield from asyncio.sleep(.01) 
 
